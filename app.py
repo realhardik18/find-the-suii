31027c6e650518417dab2c5d658fc6858dc75e4d
hardik singh <realhardik18@gmail.com>
2022-01-23 01:13:44 +0530

    added script to print mouse coordinates in console
    going to start image position generator tommorow

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/game')
+def game():
+    return render_template("game.html")
+
+
+def run():
+    app.run(host='0.0.0.0')
+
+
+def show_site():
+    t = Thread(target=run)
+    t.start()
+
+
+run()
---
+++ static/scripts/mouse.js
@@ -0,0 +1,3 @@
+window.addEventListener("mousemove", function(e) {
+    console.log(e.x + " " + e.y)
+});
